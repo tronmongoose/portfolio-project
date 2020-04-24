99508465a1340b830483734a44a1a8dcefa6da33
hariscs4997 <hariscs4997@gmail.com>
2020-04-24 20:20:26 +0500

    updated

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Blog(models.Model):
         return self.title
 
     def summary(self):
-        return body
+        return self.body[:100]
 
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
