980235bc38d766758247f1f87467326a55269783
hariscs4997 <hariscs4997@gmail.com>
2020-04-24 20:10:25 +0500

    updated

--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 from .models import Job
+from blog.models import Blog
 
 def home(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs':jobs})
+    blogs = Blog.objects.all()
+    return render(request, 'jobs/home.html', {'jobs':jobs,'blogs':blogs})
