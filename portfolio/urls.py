c191f4f6d0b38bd81c5de68975cd987a9c96c704
Erik H <51734161+tronmongoose@users.noreply.github.com>
2020-04-24 19:51:26 +0500

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,5 +7,5 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
-#     path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
